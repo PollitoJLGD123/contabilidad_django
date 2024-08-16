79a60080152634e33903e0d2292ed2c694392a43
juanAurelio03 <t1053300821@unitru.edu.pe>
2024-08-16 13:06:29 -0500

    cambie e seting

--- django_sisventas/settings.py
+++ django_sisventas/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djventas',
         'USER':'root',
-        'PASSWORD':'root',
+        'PASSWORD':'',
         'LOCALHOST':'localhost',
         'PORT':'3306'
     }
